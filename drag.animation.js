0d77f287d67dbb524f3536015db44563ac459903
Adam Giacomelli <adamg@agialab.com>
2015-06-04 12:12:04 +0200

    Updated for mobile

--- drag.animation.js
+++ drag.animation.js
@@ -2,6 +2,7 @@
 var isDragging = false;
 var initLoc = 0;
 var diffLoc = 0;
+var dragAnimation = 0;
 
 function pad(num, size) {
     var s = "000000000" + num;
@@ -9,6 +10,10 @@ function pad(num, size) {
 }
 
 function DragAnimation(imgPrefix, frameCount, imgType, speed) {
+    //Global dragAnimation
+    if(dragAnimation == 0)
+        dragAnimation = this;
+    
     this.animationFrame = 0;    
     this.currentAnimationFrame = 0;    
     this.speedFactor = speed/10;
@@ -16,6 +21,7 @@ function DragAnimation(imgPrefix, frameCount, imgType, speed) {
     this.imgType = imgType;
     this.imgPrefix = imgPrefix;
     
+    //Generate images in the drag-animation div and preload them
     this.images = new Array();
     if(!!document.getElementById("drag-animation")) {            
         for(var i = 0; i < this.frameCount; i++) {
@@ -29,7 +35,34 @@ function DragAnimation(imgPrefix, frameCount, imgType, speed) {
             document.getElementById("drag-animation").appendChild(this.images[i]);
         }
     }
+    
+    //Subscribe to events for mobile dragging
+    $("#drag-animation").bind("touchstart", function(e) {
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0]
+        isDragging = true;
+        initLoc = touch.pageX;
+        diffLoc = 0;
+        
+        $(window).bind("touchmove", function(e) {
+             if(isDragging) {            
+                var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0]
+                diffLoc = touch.pageX - initLoc;
+                var changed = dragAnimation.incrementAnimationFrame(diffLoc);
+                if(changed > 0)
+                    initLoc = touch.pageX;
+            }    
+        }); 
+    });
 
+    $(window).bind("touchend", function(e){
+        var wasDragging = isDragging;
+        isDragging = false;    
+        if (!wasDragging) { 
+            //was clicking    
+        }
+    });
+    
+    //Subscribe to events for mouse dragging
     $("#drag-animation").mousedown(function(e) {
         isDragging = true;
         initLoc = e.pageX;
